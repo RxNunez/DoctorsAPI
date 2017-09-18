bf83b9c65084e044d7fc4ae69171a26552ae7ada
RxNunez <Nunez.ryan@gmail.com>
2017-09-17 21:20:56 -0700

    include separation of function and data output

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,3 +1,5 @@
+let apiKey = require('./../.env').apiKey;
+
 var ApplicationModule = require('./../js/doctor.js').applicationModule;
 
 var displayData = function(response) {
@@ -7,19 +9,37 @@ console.log(Array.isArray(response.data));
 let doctorData = response.data;
 for(let index in doctorData){
   console.log(response.data[index].profile);
-  $('ul#solutions').append(`<li>Name: ${response.data[index].profile} <br> Web: ${response.data[index].media.url} <br> Address:${response.data[index].practices.visit_address}</li>`);
+  let Name = doctorData[index].profile.name;
+  let title = doctorData[index].profile.title;
+  let street;
+  let city;
+  let state;
+  let zip;
+  let phoneIndex = 0;
+  let phoneData;
+  let phoneNumber;
+  let website;
+  let practiceIndex=0;
+  let prac = data[index].practices;
+for(let pracIndex in prac){
+       street = prac[pracIndex].visit_address.street;
+       city = prac[pracIndex].visit_address.city;
+       state = prac[pracIndex].visit_address.state;
+       zip = prac[pracIndex].visit_address.zip;
+       $('#solutions').append(`<li>Name: ${Name}, ${title}  <br>${street} <br> ${city}, ${state},  <br> ${zip}</li>`);
   index++;
    }
-};
+}
 $(document).ready(function(){
   var applicationModule = new ApplicationModule();
-  $("#doctor").click(function(evt){
+  $(".doctor").click(function(evt){
     evt.preventDefault();
     evt.stopImmediatePropagation();
     evt.stopPropagation();
 
     let inputName = $("#name").val();
     let inputIssue = $("#issue").val();
-    applicationModule.getData(inputName, inputIssue, displayData);
+    applicationModule.getData(inputIssue, issueError, displayData);
+    applicationModule.getName(inputName, nameError, displayDocs);
  });
 });
