4c6f5b91d53d444b810e597ac9f5236e9d2c9a0f
RxNunez <Nunez.ryan@gmail.com>
2017-09-26 21:23:39 -0700

    include defining variables and html additions

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,18 @@
 var ApplicationModule = require('./../js/doctor.js').applicationModule;
 let applicationModule = new ApplicationModule();
 let displayData = function(response) {
-  // results.forEach(function(result) {
+
 let index = 0;
-console.log(Array.isArray(response.data));
+console.log((response.data));
 let doctorData = response.data;
+if(response.meta.length === 0){
+  $('#retry').append("Search request not found. Please try again.");
+} else {
 for(let index in doctorData){
   console.log(response.data[index].profile);
-  let Name = doctorData[index].profile.name;
+  // let name = doctorData[index].profile.name;
+  let lastName = doctorData[index].profile.last_name;
+  let firstName = doctorData[index].profile.first_name;
   let title = doctorData[index].profile.title;
   let street;
   let city;
@@ -16,30 +21,37 @@ for(let index in doctorData){
   let phoneIndex = 0;
   let phoneData;
   let phoneNumber;
+  let accepting;
   let website;
   let practiceIndex=0;
   let prac = data[index].practices;
-for(let pracIndex in prac){
-       street = prac[pracIndex].visit_address.street;
-       city = prac[pracIndex].visit_address.city;
-       state = prac[pracIndex].visit_address.state;
-       zip = prac[pracIndex].visit_address.zip;
-       $('#solutions').append(`<li>Name: ${Name}, ${title}  <br> ${street} <br> ${city}, ${state},  <br> ${zip}</li>`);
+for(let practiceIndex in prac){
+       street = prac[practiceIndex].visit_address.street;
+       city = prac[practiceIndex].visit_address.city;
+       state = prac[practiceIndex].visit_address.state;
+       zip = prac[practiceIndex].visit_address.zip;
+       phoneNumber = prac[practiceIndex].phones.number;
+       accepting = prac[practiceIndex].accepts_new_patients;
+       if(accepting === true){
+         accepting = "Yes";
+       } else {
+         accepting = "No";
+       }
+       $('#solutions').append(`<li>Name: ${firstName} ${lastName}, ${title}  <br> ${street} <br> ${city}, ${state}, ${zip} <br> ${phoneNumber} <br>Accepting New Patients: ${accepting}</li>`);
   index++;
    }
 }
+}
 };
 $(document).ready(function(){
-  // var applicationModule = new ApplicationModule();
   $("#doctor").click(function(evt){
     evt.preventDefault();
     evt.stopImmediatePropagation();
     evt.stopPropagation();
 
-    // let inputName = $("#name").val();
     let inputIssue = $("#issue").val();
     $("#issue").val("");
+    $("#retry").empty();
     applicationModule.getData(inputIssue, displayData);
-    // applicationModule.getName(inputName, displayData);
- });
+   });
 });
